dd353f7277dc37b3068d79ee5a7608908ad0cc3f
abdoulk9 <abdoulkadre@yahoo.fr>
2020-02-18 00:49:27 +0100

    Animated circle

---
+++ circle_move/circle.js
@@ -0,0 +1,28 @@
+
+function start() {
+    showCircle(160, 160, 100, div => {
+        div.classList.add('message');
+        div.append("Welcome my friends");
+    })
+}
+
+function showCircle(cx, cy, radius, callback) {
+    let div = document.createElement('div');
+    div.style.width = 0;
+    div.style.height = 0;
+    div.style.left = cx + 'px';
+    div.style.top = cy + 'px';
+    div.className = 'circle';
+    document.body.append(div);
+
+    setTimeout(() => {
+        div.style.width = radius * 2 + 'px';
+        div.style.height = radius * 2 + 'px';
+
+        div.addEventListener('transitionend', function handler() {
+            div.removeEventListener('transitionend', handler);
+            callback(div);
+        });
+    }, 1500);
+
+}
