ceda3028e1848475d5e3ea2a9d57e73010778b46
abdoulk9 <abdoulkadre@yahoo.fr>
2020-03-04 12:28:28 +0100

    Implementation de la function recherche un produit

--- my-shop/js/my_shop.js
+++ my-shop/js/my_shop.js
@@ -3,6 +3,7 @@ const maxQty = 10;
 const articleIdKey = "article";
 const orderIdKey = "order";
 const inputIdKey = "qty";
+var quantityId = "";
 const catalog = [
     {
         "name": "Abricot",
@@ -93,6 +94,7 @@ var createShop = function () {
     }
 }
 
+
 /* Create a div article*/
 const createProduct = function (article, index) {
     const block = document.createElement("div");
@@ -106,7 +108,7 @@ const createProduct = function (article, index) {
     return block;
 }
 
-/* Craate a figure block for a article*/
+/* Create a figure block for a article*/
 const createFigureBlock = function (article) {
     let figure = document.createElement("figure");
     let img = document.createElement('img');
@@ -129,10 +131,12 @@ const createBlock = function (tag, content, cssClass) {
     return element;
 }
 
-/* Create a  */
+
+/* Create a control order */
 const createOrderControlBlock = function (index) {
     var control = document.createElement("div");
     control.className = "control";
+
     //Create input qty element
     var input = document.createElement("input");
     input.id = index + "-" + inputIdKey;
@@ -142,6 +146,7 @@ const createOrderControlBlock = function (index) {
     input.min = "0";
     input.max = maxQty.toString();
     control.appendChild(input);
+
     //Create order button
     var button = document.createElement("button");
     button.className = "commander";
@@ -149,3 +154,61 @@ const createOrderControlBlock = function (index) {
     control.appendChild(button);
     return control;
 }
+
+const activeButtonCommander = function (element) {
+    return element.style.opacity = "opacity : 1";
+}
+
+
+const createPurchase = function (article, index) {
+    var block = document.createElement('div');
+    block.className = "purchase";
+    block.id = index + "-" + articleIdKey;
+    block.appendChild(createFigureBlock(article));
+    block.appendChild(createBlock("h4", article.name));
+    block.appendChild(createDiv("div", "qty"));
+    block.appendChild(createDiv("div", "prix"));
+    var purchases = document.getElementById("purchases");
+    this.appendChild(block);
+    return purchases;
+}
+const createDiv = function (tag, name) {
+    var element = document.createElement(tag);
+    element.className = name;
+    return element;
+}
+
+
+
+
+/* Function to search a article*/
+const input = document.querySelector("input");
+const filter = document.getElementById("filter");
+const shop = document.getElementById("shop");
+
+filter.addEventListener("keyup", function () {
+    let catalogFilter = [];
+    let colorDisplay;
+    for (var i = 0; i < catalog.length; i++) {
+        if (catalog[i].name.indexOf(filter.value) === 0) {
+            catalogFilter.push(catalog[i]);
+            colorDisplay = "green";
+            input.style.backgroundColor = colorDisplay;
+        } else if (catalog[i].name.indexOf(filter.value) === -1) {
+            colorDisplay = "red";
+            input.style.backgroundColor = colorDisplay;
+            input.textContent = "";
+            init();
+        } else {
+            colorDisplay = "white";
+            input.style.backgroundColor = colorDisplay;
+            init();
+        }
+    }
+    for (var i = 0; i < catalogFilter.length; i++) {
+        console.log(catalogFilter[i].name);
+        shop.innerHTML = "";
+        shop.appendChild(createProduct(catalogFilter[i], i));
+    }
+});
+
